585be5dfa9b51cbe3eb92a53334b1796a4128b07
Naufan Rizal <naufan@buildkite.com>
2024-09-27 12:17:07 +1200

    Parse Playwright result

--- internal/runner/playwright.go
+++ internal/runner/playwright.go
@@ -1,7 +1,10 @@
 package runner
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 
@@ -43,7 +46,45 @@ func (p Playwright) Run(testCases []string, retry bool) (RunResult, error) {
 		return RunResult{Status: RunStatusPassed}, nil
 	}
 
-	return RunResult{}, nil
+	if ProcessSignaledError := new(ProcessSignaledError); errors.As(err, &ProcessSignaledError) {
+		return RunResult{Status: RunStatusError}, err
+	}
+
+	if exitError := new(exec.ExitError); errors.As(err, &exitError) {
+		report, parseErr := p.parseReport(p.ResultPath)
+		if parseErr != nil {
+			fmt.Println("Buildkite Test Engine Client: Failed to read Playwright output, tests will not be retried.")
+			return RunResult{Status: RunStatusError}, err
+		}
+
+		if report.Stats.Unexpected > 0 {
+			var failedTests []string
+			for _, suite := range report.Suites {
+				for _, spec := range suite.Specs {
+					if !spec.Ok {
+						failedTests = append(failedTests, fmt.Sprintf("%s:%d", spec.File, spec.Line))
+					}
+				}
+			}
+			return RunResult{Status: RunStatusFailed, FailedTests: failedTests}, nil
+		}
+	}
+
+	return RunResult{Status: RunStatusError}, err
+}
+
+func (p Playwright) parseReport(path string) (PlaywrightReport, error) {
+	var report PlaywrightReport
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return PlaywrightReport{}, fmt.Errorf("failed to read playwright output: %v", err)
+	}
+
+	if err := json.Unmarshal(data, &report); err != nil {
+		return PlaywrightReport{}, fmt.Errorf("failed to parse playwright output: %s", err)
+	}
+
+	return report, nil
 }
 
 func (p Playwright) GetFiles() ([]string, error) {
@@ -52,4 +93,26 @@ func (p Playwright) GetFiles() ([]string, error) {
 
 func (p Playwright) GetExamples(files []string) ([]plan.TestCase, error) {
 	return nil, nil
+}
+
+type PlaywrightSpec struct {
+	File   string
+	Line   int
+	Column int
+	Id     string
+	Title  string
+	Ok     bool
+}
+
+type PlaywrightReportSuite struct {
+	Title string
+	Specs []PlaywrightSpec
+}
+
+type PlaywrightReport struct {
+	Suites []PlaywrightReportSuite
+	Stats  struct {
+		Expected   int
+		Unexpected int
+	}
 }
--- internal/runner/playwright_test.go
+++ internal/runner/playwright_test.go
@@ -10,8 +10,7 @@ func TestPlaywrightRun(t *testing.T) {
 	mockCwd(t, "./fixtures/playwright")
 
 	playwright := NewPlaywright(RunnerConfig{
-		TestCommand: "yarn run playwright test",
-		ResultPath:  "playwright.json",
+		ResultPath: "playwright.json",
 	})
 
 	files := []string{"./fixtures/playwright/tests/example.spec.js"}
@@ -19,6 +18,30 @@ func TestPlaywrightRun(t *testing.T) {
 
 	want := RunResult{
 		Status: RunStatusPassed,
+	}
+
+	if err != nil {
+		t.Errorf("Playwright.Run(%q) error = %v", files, err)
+	}
+
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("Playwright.Run(%q) diff (-got +want):\n%s", files, diff)
+	}
+}
+
+func TestPlaywrightRun_Failed(t *testing.T) {
+	mockCwd(t, "./fixtures/playwright")
+
+	playwright := NewPlaywright(RunnerConfig{
+		ResultPath: "./test-results/results.json",
+	})
+
+	files := []string{"./fixtures/playwright/tests/failed.spec.js", "./fixtures/playwright/tests/example.spec.js"}
+	got, err := playwright.Run(files, false)
+
+	want := RunResult{
+		Status:      RunStatusFailed,
+		FailedTests: []string{"failed.spec.js:3"},
 	}
 
 	if err != nil {
